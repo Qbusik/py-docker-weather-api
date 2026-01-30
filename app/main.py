67880d50ef0415ed158189e3aee93dd455915dcd
Jakub Paluch <qbusik@gmail.com>
2026-01-30 07:44:22 +0100

    Flake8 changes

--- app/main.py
+++ app/main.py
@@ -8,17 +8,19 @@ FILTERING = "Paris"
 
 KEY = os.getenv("API_KEY")
 
+
 def get_weather() -> None:
     if not KEY:
         raise Exception("Weather API key not found!")
     result = requests.get(URL + f"q={FILTERING}&key={KEY}")
     data = result.json()
     print(
-        f"{data['location']['name']}/{data['location']['country']} "
-        f"{data['location']['localtime']}\n"
-        f"Weather: {data['current']['temp_c']}°C, " +
-        data['current']['condition']['text']
+        f"{data["location"]["name"]}/{data["location"]["country"]} "
+        f"{data["location"]["localtime"]}\n"
+        f"Weather: {data["current"]["temp_c"]}°C, "
+        + data["current"]["condition"]["text"]
     )
+
 
 if __name__ == "__main__":
     get_weather()
